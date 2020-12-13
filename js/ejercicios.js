87cb961892f1682d6e4d173c7a14bfb42209e43a
Slaya M. Ostapczuk <slaya.ostapczuk@gmail.com>
2020-12-13 14:57:17 -0300

    hice los ejercicios que faltaban pero sin entender mucho. Me puse a leer de nuevo para ver si los entendía

--- js/ejercicios.js
+++ js/ejercicios.js
@@ -9,7 +9,7 @@ let numero2 = parseInt(prompt("Ingrese el segundo número"));
 document.write("Número 1: " + numero1);
 document.write("<br>Número 2: " + numero2);
 document.write("<br>Resultado: " + (numero1 + numero2));*/
-let numero1 = parseInt(prompt("Ingrese el primer número"));
+/*(ejercicio6)let numero1 = parseInt(prompt("Ingrese el primer número"));
 let numero2 = parseInt(prompt("Ingrese el segundo número"));
 
 document.write("Número 1: " + numero1);
@@ -19,4 +19,50 @@ if (numero1 > numero2) {
     document.write("<br>El número 1 es mayor");
 } else {
     document.write("<br>El número 2 es mayor");
+}*/
+/*(ejercicio7)let numero1 = parseInt(prompt('Primer número: '));
+let numero2 = parseInt(prompt('Segundo número: '));
+let numero3 = parseInt(prompt('Tercer número: '));
+
+if ((numero1 == numero2) && (numero1 == numero3)) document.write('Los tres números son iguales.');
+else if ((numero1 > numero2) && (numero1 > numero3)) document.write('El número más grande es: ' + numero1);
+else if ((numero2 > numero1) && (numero2 > numero3)) document.write('El número más grande es: ' + numero2);
+else document.write('El número más grande es: ' + numero3);*/
+/*(ejercico8)let num = parseInt(prompt('Número: '));
+
+if (num % 2 == 0) document.write('El número ' + num + ' es divisible por 2.');
+else document.write('El número ' + num + ' no es divisible por 2.');*/
+/*(ejercicio9)let userString = prompt('Frase:');
+let auxString = '';
+
+for (let i = 0; i < userString.length; i++) {
+    switch (userString[i].toLowerCase()) {
+        case 'a':
+            auxString += userString.substr(i, 1);
+            break;
+        case 'e':
+            auxString += userString.substr(i, 1);
+            break;
+        case 'i':
+            auxString += userString.substr(i, 1);
+            break;
+        case 'o':
+            auxString += userString.substr(i, 1);
+            break;
+        case 'u':
+            auxString += userString.substr(i, 1);
+            break;
+        case ' ':
+            auxString += userString.substr(i, 1);
+            break;
+    }
 }
+
+document.write(auxString);*/
+/*(ejercicio10)let userNum = parseInt(prompt('Número:'));
+
+if (userNum % 2 == 0) document.write('El número es divisible por 2.');
+else if (userNum % 3 == 0) document.write('El número es divisible por 3.');
+else if (userNum % 5 == 0) document.write('El número es divisible por 5.');
+else if (userNum % 7 == 0) document.write('El número es divisible por 7.');
+else document.write('El número no es divisible por 2, 3, 5 o 7.');*/
