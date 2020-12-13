ed71804110c0ffcf178210ea11c86b86a872e433
Slaya M. Ostapczuk <slaya.ostapczuk@gmail.com>
2020-12-13 14:35:37 -0300

    fue bastante faciel, y creo que parseInt, tiene que ver con la logica y numeros

--- js/ejercicios.js
+++ js/ejercicios.js
@@ -1,5 +1,11 @@
 //(ejercicio1)alert("Un mensaje");
 //(ejercicio2)document.write("Hello World");
 //(ejercicio3)document.write("3+5");
-let nombreusuario = prompt("Nombre de usuario");
-document.write("Hola " + nombreusuario);
+/*(ejercicio4)let nombreusuario = prompt("Nombre de usuario");
+document.write("Hola " + nombreusuario);*/
+/*(ejercico5)let numero1 = parseInt(prompt("Ingrese el primer número"));
+let numero2 = parseInt(prompt("Ingrese el segundo número"));
+
+document.write("Número 1: " + numero1);
+document.write("<br>Número 2: " + numero2);
+document.write("<br>Resultado: " + (numero1 + numero2));*/
